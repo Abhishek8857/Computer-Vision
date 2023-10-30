24c9810fd0a93d8d26aedf4fae9589eab105ce37
Abhishek Deshmukh <deshmukhabhi02@gmail.com>
2023-10-30 16:35:44 +0100

    Update Local_Features.py

--- Local_Features.py
+++ Local_Features.py
@@ -146,9 +146,11 @@ def compute_1d_histogram(im, n_bins):
     return histogram
 
 
+# 1D Histogram Plot 
+
 fig, axes = plt.subplots(1, 4, figsize=(10, 2), constrained_layout=True)
 bin_counts = [2, 25, 256]
-gray_img = iio.imread("images\Cat.jpg", mode="L").astype(np.float32) / 256
+gray_img = iio.imread("images/terrain.png", mode="L").astype(np.float32) / 256
 
 axes[0].set_title("Image")
 axes[0].imshow(gray_img, cmap="gray")
@@ -183,7 +185,9 @@ def plot_3d_histogram(ax, data, axis_names="xyz"):
     ax.set_ylabel(axis_names[1])
     ax.set_zlabel(axis_names[2])
 
-paths = ["images\Marq_2.jpg", "images\Quart_1.jpg"]
+# 3D Histogram Plot
+
+paths = ["images/Marq_2.jpg", "images/Quart_1.jpg"]
 images = [iio.imread(p) for p in paths]
 plot_multiple(images, paths)
 
@@ -209,7 +213,7 @@ def compute_2d_histogram(im, n_bins):
 
 def compute_gradient_histogram(rgb_im, n_bins):
     # Convert to grayscale
-    gray_im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY).astype(float)
+    gray_im = cv2.cvtColor(rgb_im, cv2.COLOR_RGB2GRAY).astype(float)
     # Compute Gaussian derivatives
     dx, dy = gauss_derivs(gray_im, sigma=2.0)
     # Map the derivatives between -10 and 10 to be between 0 and 1
@@ -221,7 +225,10 @@ def compute_gradient_histogram(rgb_im, n_bins):
     return dx, dy, compute_2d_histogram(gradients, n_bins=16)
 
 
-paths = ["images\Marq_2.jpg", "images\Quart_1.jpg"]
+# Gradient Histogram Plot
+
+paths = ["images/Marq_2.jpg", "images/Quart_1.jpg"]
+# paths = ["images/model/obj4__0.png", "image/model/obj42__0.png"]
 images, titles = [], []
 
 for path in paths:
@@ -231,4 +238,77 @@ for path in paths:
     titles += [path, "dx", "dy", "Histogram (log)"]
 
 plot_multiple(images, titles, max_columns=4, imwidth=2, imheight=2, colormap="viridis")
+
+
+def compute_maglap_histogram(rgb_im, n_bins):
+    # Convert to grayscale
+    gray_im = cv2.cvtColor(rgb_im, cv2.COLOR_RGB2GRAY).astype(float)
+    # Compute Gaussian derivatives
+    sigma = 2
+    kernel_radius = np.ceil(3.0 * sigma)
+    x = np.arange(-kernel_radius, kernel_radius + 1)[np.newaxis]
+    G = gauss(x, sigma)
+    D = gaussdx(x, sigma)
+    dx = convolve_with_two(gray_im, D, G.T)
+    dy = convolve_with_two(gray_im, G, D.T)
+
+    # Compute second derivatives
+    dxx = convolve_with_two(dx, D, G.T)
+    dyy = convolve_with_two(dy, G, D.T)
+
+    # Compute gradient magnitude and Laplacian
+    mag = np.sqrt(dxx**2 + dyy**2)
+    lap = dxx + dyy
+    mag = map_range(mag, start=0, end=15)
+    lap = map_range(lap, start=-5, end=5)
+    
+    mag_lap = np.stack([mag, lap], axis=-1)
+
+    return mag, lap, compute_2d_histogram(mag_lap, n_bins=16)
+
+# Gradient Histogram Plot
+
+paths = [f"images/model/obj{i}__0.png" for i in [20, 37, 36, 55]]
+images, titles = [], []
+
+for path in paths:
+    im = iio.imread(path)
+    mag, lap, hist = compute_maglap_histogram(im, n_bins=16)
+    images += [im, mag, lap, np.log(hist + 1e-3)]
+    titles += [path, "Gradient magn.", "Laplacian", "Histogram (log)"]
+plot_multiple(images, titles, imwidth=2, imheight=2, max_columns=4, colormap="viridis")
+
+
+def euclidean_distance(hist1, hist2):
+    distances = np.sqrt(np.sum(np.subtract(hist1, hist2)**2))
+    return np.round(distances, 3)
+
+
+def chi_square_distance(hist1, hist2, eps=1e-3):
+    num = np.subtract(hist1, hist2)**2
+    den = np.add(np.add(hist1, hist2), eps)
+    distances = 0.5*np.sum(np.divide(num, den))
+    return np.round(distances, 3)
+
+im1 = iio.imread("images/model/obj1__0.png")
+im2 = iio.imread("images/model/obj91__0.png")
+im3 = iio.imread("images/model/obj94__0.png")
+
+n_bins = 8
+h1 = compute_3d_histogram(im1 / 256, n_bins)
+h2 = compute_3d_histogram(im2 / 256, n_bins)
+h3 = compute_3d_histogram(im3 / 256, n_bins)
+
+eucl_dist1 = euclidean_distance(h1, h2)
+chisq_dist1 = chi_square_distance(h1, h2)
+eucl_dist2 = euclidean_distance(h1, h3)
+chisq_dist2 = chi_square_distance(h1, h3)
+
+titles = [
+    "Reference image",
+    f"Eucl: {eucl_dist1:.3f}, ChiSq:  {chisq_dist1:.3f}",
+    f"Eucl: {eucl_dist2:.3f}, ChiSq:  {chisq_dist2:.3f}",
+]
+
+plot_multiple([im1, im2, im3], titles, imheight=3)
 plt.show()
